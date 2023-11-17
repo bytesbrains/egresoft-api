31c772b49d53d7d15dbdfe6a5095518d63d76ea8
gustom <101676957+gust0m@users.noreply.github.com>
2023-11-17 05:21:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from routers import user,egresado
+from routers import user,usersdb
 
 load_dotenv()
 
@@ -8,6 +9,8 @@ app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(egresado.router)
+app.include_router(user.router)
+app.include_router(usersdb.router)
 
 @app.get("/")
 def read_root():
