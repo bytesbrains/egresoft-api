26d0e7239a10d842668ee46f82fe0dc8a0a428ac
mrgabdev <gabriel.escobar.torres@gmail.com>
2023-11-27 09:43:29 -0600

    fix: :bug: fix cors error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import (
     user,
     egresado,
@@ -14,6 +15,22 @@ from routers import (
 load_dotenv()
 
 app = FastAPI()
+
+# Configurar los orígenes permitidos, métodos, encabezados, etc.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Reemplaza con los orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Rutas de tu aplicación FastAPI
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World"}
+
 
 app.include_router(user.router)
 app.include_router(administrativo.router)
