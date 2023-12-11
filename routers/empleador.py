cee88d748ec5036661144a106b5198af3e876074
gust0m <joseadriantorres2002@gmail.com>
2023-12-10 23:26:46 -0600

    Creacion de rutas get y put  para employer

---
+++ routers/empleador.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import SQLAlchemyError
+from models.models import Base, EmpleadorBasico
+from database.database import engine, get_db
+from sqlalchemy.orm import Session
+from utils.helper_functions import get_empleador
+
+Base.metadata.create_all(bind=engine)
+
+router = APIRouter(
+    prefix="/empleador",
+    tags={"Empleador"},
+    responses={404: {"message": "No encontrado"}},
+)
+
+
+@router.get("/get/{id_emp}")
+async def return_empleador(id_emp: str, db: Session = Depends(get_db)):
+    try:
+        return db.query(EmpleadorBasico).filter(EmpleadorBasico.id_emp == id_emp).one()
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="Empleador no encontrado")
+
+
+@router.put("/{id_adm}")
+async def actualizar_empleador(
+    id_emp: str, updated_data: EmpleadorBasico, db: Session = Depends(get_db)
+):
+    try:
+        empleador_actual = await get_empleador(db, id_emp)
+
+        # Actualiza solo los datos ingresados
+        for key, value in updated_data.dict(exclude_unset=True).items():
+            setattr(empleador_actual, key, value)
+
+        db.commit()
+        return {"message": "Datos del Empleador actualizados correctamente"}
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+    finally:
+        db.close()
